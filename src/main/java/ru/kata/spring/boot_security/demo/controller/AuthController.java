66c8f2fc9b807cb1ac8d4bac323d0721f0d8d462
Vladislav <vladikul98@gmail.com>
2024-02-22 23:10:58 +0300

    1.0.1 ver

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AuthController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AuthController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 @RequestMapping("/login")
 public class AuthController {
